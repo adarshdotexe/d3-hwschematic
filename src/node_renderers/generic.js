cee20c080122f27385e154b2f256e82c612c82f4
Adarsh Varshney <92206228+adarshdotexe@users.noreply.github.com>
2024-05-20 00:48:14 +0530

    Update generic.js

--- src/node_renderers/generic.js
+++ src/node_renderers/generic.js
@@ -1,317 +1,342 @@
 import * as d3 from "d3";
-import {addMarkers} from "./markers";
-import {NodeRendererContainer} from "./nodeRendererContainer";
-import {OperatorNodeRenderer} from "./node_renderers/operatorNode";
-import {MuxNodeRenderer} from "./node_renderers/muxNode";
-import {SliceNodeRenderer} from "./node_renderers/sliceNode";
-import {GenericNodeRenderer} from "./node_renderers/generic";
-import {renderLinks} from "./linkRenderer";
-import {Tooltip} from "./tooltip";
-import {yosys} from "./yosys.js";
-import {
-    hyperEdgesToEdges,
-    getNet, initNodeParents, expandPorts
-} from "./dataPrepare";
-import {default as d3elk} from "./elk/elk-d3";
-import {selectGraphRootByPath} from "./hierarchySelection.js";
+import { schemePuBuGn } from "d3";
+import { getIOMarker } from "../markers";
 
-function getNameOfEdge(e) {
-    let name = "<tspan>unnamed</tspan>";
-    if (e.hwMeta) {
-        if (typeof e.hwMeta.name === "undefined") {
-            let p = e.hwMeta.parent;
-            let pIsHyperEdge = typeof p.sources !== "undefined"
-            if (pIsHyperEdge && p.hwMeta) {
-                name = p.hwMeta.name;
-            }
-        } else {
-            name = e.hwMeta.name;
-        }
-    }
-    return name;
+
+function portLevel(port){
+	if(!port.parent) return 0;
+	else{
+		return portLevel(port.parent)+1;
+	}
+
 }
 
-function toggleHideChildren(node) {
-    let children;
-    let nextFocusTarget;
-    if (node.children) {
-        // children are visible, will collapse
-        children = node.children;
-        nextFocusTarget = node.hwMeta.parent;
-    } else {
-        // children are hidden, will expand
-        children = node._children;
-        nextFocusTarget = node;
-    }
-
-    let tmpChildren = node.children;
-    node.children = node._children;
-    node._children = tmpChildren;
-    let tmpEdges = node.edges;
-    node.edges = node._edges;
-    node._edges = tmpEdges;
-    node.hwMeta.renderer.prepare(node);
-    return [children, nextFocusTarget];
-}
-
-/**
- * HwScheme builds scheme diagrams after bindData(data) is called
- *
- * @param svg: root svg element where scheme will be rendered
- * @attention zoom is not applied it is only used for focusing on objects
- * @note do specify size of svg to have optimal result
+/*
+ * Basic renderer which renders node as a box with ports, optionally with the body text
  */
-export default class HwSchematic {
-    constructor(svg) {
-        // flag for performance debug
-        this._PERF = false;
-        // main svg element
-        this.svg = svg;
-        // default sizes of elements
-        this.PORT_PIN_SIZE = [7.55, 13];
-        this.PORT_HEIGHT = this.PORT_PIN_SIZE[1] + 1;
-        this.PORT_WIDTH = this.PORT_PIN_SIZE[0];
-        this.CHAR_HEIGHT = this.PORT_PIN_SIZE[1];
-        this.CHAR_WIDTH = this.PORT_PIN_SIZE[0];
-        this.NODE_MIDDLE_PORT_SPACING = 50;
-        this.MAX_NODE_BODY_TEXT_SIZE = [400, 400];
-        // top, right, bottom, left
-        this.BODY_TEXT_PADDING = [15, 10, 0, 10];
-        svg.classed("d3-hwschematic", true);
-        this.defs = svg.append("defs");
-        this.root = svg.append("g");
-        this.errorText = null;
-        this._nodes = null;
-        this._edges = null;
+export class GenericNodeRenderer {
+    /**
+     * @param schematic instance of HwSchematic
+     **/
+	constructor(schematic) {
+		this.schematic = schematic;
+	}
+    /**
+     * check if this selector should be used for this node
+     **/
+	selector(node) {
+		// always return true, because this is a default renderer which just renders a box with ports
+		return true;
+	}
 
-        // graph layouter to resolve positions of elements
-        this.layouter = new d3elk();
-        this.layouter
-            .options({
-                edgeRouting: "ORTHOGONAL",
-
-            })
-            .transformGroup(this.root);
-
-        // shared tooltip object
-        this.tooltip = new Tooltip(document.getElementsByTagName('body')[0]);
-
-        // renderer instances responsible for rendering of component nodes
-        this.nodeRenderers = new NodeRendererContainer();
-        addMarkers(this.defs, this.PORT_PIN_SIZE);
-        let rs = this.nodeRenderers;
-        rs.registerRenderer(new GenericNodeRenderer(this));
-    }
-
-    widthOfText(text) {
-        if (text) {
-            return text.length * this.CHAR_WIDTH;
-        } else {
-            return 0;
-        }
-    }
-
-    removeGraph() {
-        this.root.selectAll("*").remove();
-    }
-
-    updateGlobalSize() {
-        let width = parseInt(this.svg.style("width") || this.svg.attr("width"), 10);
-        let height = parseInt(this.svg.style("height") || this.svg.attr("height"), 10);
-
-        this.layouter
-            .size([width, height]);
-    }
+	getNodeLabelWidth(d) {
+		var schematic = this.schematic;
+		var widthOfText = schematic.widthOfText.bind(schematic);
+		return widthOfText(d.hwMeta.name);
+	}
 
     /**
-     * Set bind graph data to graph rendering engine
+     * Init bodyText and resolve size of node from body text and ports
      *
-     * @return promise for this job
+     * @param d component node
+     *
      */
-    bindData(graph) {
-        this.removeGraph();
-        // let postCompaction = "layered.compaction.postCompaction.strategy";
-        // if (!graph.properties[postCompaction]) {
-        //     graph.properties[postCompaction] = "NONE";
-        // }
-        hyperEdgesToEdges(graph, graph.hwMeta.maxId);
-        initNodeParents(graph, null);
-        expandPorts(graph);
+	initNodeSizes(d) {
+		var schematic = this.schematic;
+		if (d.properties["org.eclipse.elk.noLayout"])
+			return;
+		var widthOfText = schematic.widthOfText.bind(schematic);
 
-        if (this._PERF) {
-            let t0 = new Date().getTime();
-            this.nodeRenderers.prepare(graph);
-            let t1 = new Date().getTime();
-            console.log("> nodeRenderers.prepare() : " + (t1 - t0) + " ms");
-        } else {
-            // nodes are ordered, children at the end
-            this.nodeRenderers.prepare(graph);
-        }
-        this.layouter
-            .kgraph(graph);
-        return this._draw();
-    }
-    /*
-    * @returns subnode selected by path wrapped in a new root
-    * */
-    static selectGraphRootByPath(graph, path) {
-        return selectGraphRootByPath(graph, path);
-    }
-    /*
-     * Resolve layout and draw a component graph from layout data
-     */
-    _draw() {
-        this.updateGlobalSize();
+		var labelW = this.getNodeLabelWidth(d);
+		var max = Math.max
+		var bodyTextSize = this.initBodyTextLines(d);
+		const MBT = schematic.MAX_NODE_BODY_TEXT_SIZE;
+		bodyTextSize[0] = Math.min(bodyTextSize[0], MBT[0]);
+		bodyTextSize[1] = Math.min(bodyTextSize[1], MBT[1]);
 
-        let layouter = this.layouter;
-        this._nodes = layouter.getNodes().slice(1) // skip root node
-        this._edges = layouter.getEdges();
-        let t0;
-        if (this._PERF) {
-            t0 = new Date().getTime();
-        }
-        let _this = this;
-        return layouter.start()
-            .then(
-                function (g) {
-                    if (_this._PERF) {
-                        let t1 = new Date().getTime();
-                        console.log("> layouter.start() : " + (t1 - t0) + " ms");
-                        t0 = t1;
-                    }
-                    _this._applyLayout(g);
-                    if (_this._PERF) {
-                        let t1 = new Date().getTime();
-                        console.log("> HwSchematic._applyLayout() : " + (t1 - t0) + " ms");
-                    }
-                },
-                function (e) {
-                    // Error while running d3-elkjs layouter
-                    throw e;
-                }
-            );
-    }
+		// {PortSide: (portCnt, portWidth)}
+		var portDim = {
+			"WEST": [0, 0],
+			"EAST": [0, 0],
+			"SOUTH": [0, 0],
+			"NORTH": [0, 0]
+		};
+		var PORT_PIN_SIZE_x = schematic.PORT_PIN_SIZE[0],
+			PORT_PIN_SIZE_y = schematic.PORT_PIN_SIZE[1];
+		var CHAR_WIDTH = schematic.CHAR_WIDTH;
+		if (d.ports != null)
+			d.ports.forEach(function(p) {
+				var t = p.side;
+				var indent = 0;
+				if (portLevel(p) > 0)
+					indent = (portLevel(p)+1) * CHAR_WIDTH;
+				var portW = widthOfText(p.hwMeta.name) + indent;
+				var pDim = portDim[t];
+				if (pDim === undefined)
+					throw new Error(t);
+				pDim[0]++;
+				pDim[1] = max(pDim[1], portW);
+
+				// dimension of connection pin
+				p.width = PORT_PIN_SIZE_x;
+				p.height = PORT_PIN_SIZE_y;
+			})
+
+		var west = portDim["WEST"],
+			east = portDim["EAST"],
+			south = portDim["SOUTH"],
+			north = portDim["NORTH"];
+
+		var portColums = 0;
+		if (west[0] && west[1] > 0)
+			portColums += 1;
+		if (east[0] && east[1] > 0)
+			portColums += 1;
+
+		var middleSpacing = 0;
+		if (portColums == 2)
+			middleSpacing = schematic.NODE_MIDDLE_PORT_SPACING
+		var portW = max(west[1], east[1]);
+
+		d.portLabelWidth = portW;
+		d.width = max(portW * portColums + middleSpacing, labelW,
+			(south[0] + north[0]) * schematic.PORT_HEIGHT)
+			+ bodyTextSize[0] + CHAR_WIDTH;
+		d.height = max((west[0] + east[0]) * schematic.PORT_HEIGHT,
+			bodyTextSize[1],
+			max(south[1], north[1]) * CHAR_WIDTH);
+
+	}
 
     /**
-     * Draw a component graph from layout data
+     * Split bodyText of one to lines and resolve dimensions of body text
+     *
+     * @param d component node
      */
-    _applyLayout() {
-        let root = this.root;
+	initBodyTextLines(d) {
+		var schematic = this.schematic;
+		var max = Math.max;
+		var bt = d.hwMeta.bodyText
+		if (bt) {
+			if (typeof bt === "string") {
+				bt = d.hwMeta.bodyText = bt.split("\n");
+			}
+			var bodyTextW = 0;
+			bt.forEach(function(line) {
+				bodyTextW = max(bodyTextW, line.length);
+			})
+			bodyTextW *= schematic.CHAR_WIDTH;
+			var bodyTextH = bt.length * schematic.CHAR_HEIGHT;
+		} else {
+			var bodyTextW = 0;
+			var bodyTextH = 0;
+		}
+		var pad = schematic.BODY_TEXT_PADDING;
+		if (bodyTextW > 0)
+			bodyTextW += pad[1] + pad[3];
+		if (bodyTextH > 0)
+			bodyTextH += pad[0] + pad[2];
+		return [bodyTextW, bodyTextH];
+	}
 
-        let node = root.selectAll(".node")
-            .data(this._nodes)
-            .enter()
-            .append("g");
-        this.nodeRenderers.render(root, node);
+    /**
+     * @param bodyTexts list of strings
+     */
+	renderTextLines(bodyTexts) {
+		var schematic = this.schematic;
+		const padTop = schematic.BODY_TEXT_PADDING[0];
+		const padLeft = schematic.BODY_TEXT_PADDING[3];
+		const MBT = schematic.MAX_NODE_BODY_TEXT_SIZE;
+		const CHAR_WIDTH = schematic.CHAR_WIDTH;
+		const CHAR_HEIGHT = schematic.CHAR_HEIGHT;
 
-        let _this = this;
-        node.on("click", function (ev, d) {
-            let [children, nextFocusTarget] = toggleHideChildren(d);
-            if (!children || children.length === 0) {
-                return; // does not have anything to expand
-            }
-            _this.layouter.markLayoutDirty();
-            _this.removeGraph();
-            _this._draw().then(
-                function () {
-                    _this.layouter.zoomToFit(nextFocusTarget);
-                },
-                function (e) {
-                    // Error while applying of layout
-                    throw e;
+		bodyTexts.each(function() {
+			var bodyText = d3.select(this)
+			var d = bodyText.data()[0];
+			var bodyTextLines = d.hwMeta.bodyText;
+			var _MBT = [MBT[0] / CHAR_WIDTH, MBT[1] / CHAR_HEIGHT];
+
+			if (bodyTextLines && (!d.children
+				|| d.children.length == 0)) {
+				bodyTextLines.forEach(function(line, dy) {
+					if (line.length > _MBT[0])
+						line = line.slice(0, _MBT[0] - 3) + "...";
+					if (dy > _MBT[1])
+						return;
+					bodyText
+						.append("tspan")
+						.attr("x", d.portLabelWidth + padLeft)
+						.attr("y", padTop)
+						.attr("dy", dy + "em")
+						.text(line);
+				});
+			}
+		});
+	}
+
+    /**
+     * Prepare node before ELK processing
+     * */
+	prepare(node) {
+		this.initNodeSizes(node)
+	}
+
+    /**
+     * Render svg of node
+     *
+     * @param root root svg element where nodes should be rendered
+     * @param nodeG svg g for each node with data binded
+     * */
+	render(root, nodeG) {
+		var node = nodeG
+			.attr("class", function(d) {
+				var cssClass;
+				if (d.hwMeta && d.hwMeta.isExternalPort) {
+					cssClass = "node-external-port";
+				} else {
+                    let depth = 0;
+                    let parent = d.hwMeta.parent;
+                    while (parent) {
+                        ++depth;
+                        parent = parent.hwMeta.parent;
+                    }
+                    if (depth % 2 === 0) {
+                        cssClass = "node node-0";
+                    } else {
+                        cssClass = "node node-1";
+                    }
+				}
+				if (d.hwMeta.cssClass) {
+					cssClass += " " + d.hwMeta.cssClass;
+				}
+				return cssClass;
+			})
+			.attr("style", function(d) { return d.hwMeta.cssStyle; });
+		var nodeBody = node.append("rect");
+		// set dimensions and style of node
+		nodeBody
+			.attr("width", function(d) { return d.width })
+			.attr("height", function(d) { return d.height })
+			// .attr("rx", 5) // rounded corners
+			// .attr("ry", 5);
+
+		// apply node positions
+		node
+			.attr("transform", function(d) {
+				if (typeof d.x === "undefined" || typeof d.x === "undefined") {
+					throw new Error("Node with undefined position", d);
+				}
+				return "translate(" + d.x + " " + d.y + ")"
+			});
+
+		// spot node label
+		var nodeLablel = node.append("text")
+			.text(function(d) {
+				if (d.hwMeta && !d.hwMeta.isExternalPort) {
+					return d.hwMeta.name;
+				} else {
+					return "";
+				}
+			});
+		nodeLablel.attr("transform", "translate(0,-" + this.schematic.CHAR_HEIGHT * 0.25 + ")")
+		// spot node body text
+		node.append("text")
+			.call(this.renderTextLines.bind(this));
+
+		this.renderPorts(node);
+	}
+
+	renderPorts(node) {
+
+		var schematic = this.schematic;
+		var PORT_HEIGHT = schematic.PORT_HEIGHT;
+		var PORT_WIDTH = schematic.PORT_WIDTH;
+		var CHAR_WIDTH = schematic.CHAR_WIDTH;
+		var portG = node.selectAll(".port")
+			.data(function(d) {
+				return d.ports || [];
+			})
+			.enter()
+			.append("g")
+			.attr("style", (d) => d.hwMeta.cssStyle)
+			.attr("class", (d) => {
+				if (d.hwMeta.cssClass) {
+					return "port " + d.hwMeta.cssClass;
+				} else {
+					return "port";
+				}
+			});
+			portG.attr("transform", function(d) {
+				return "translate(" + d.x  + "," + d.y + ")";
+			});
+
+		node.each(function(d) {
+			var ignorePortLabel = typeof d.children !== "undefined";
+			if (d.ports != null) {
+				d.ports.forEach(function(p) {
+					p.ignoreLabel = ignorePortLabel;
+				});
+			}
+		})
+
+		// spot port name
+		portG.append("text")
+			.text(function(d, i) {
+                /*var next_d = port_data[i+1];
+                if (next_d && next_d.hwMeta.level > d.hwMeta.level) {
+					console.log(d.hwMeta.name);
+                    //d.hwMeta.name=toString("+");
                 }
-            );
-        });
+                */
+				if (d.ignoreLabel)
+					return "";
+				else if (d.parent) {
+					var indent = '-'.repeat(portLevel(d));
+					// var side = d.side;
+					if (d.x < 0) {
+						var side = "WEST"
+					} else {
+						var side = "EAST"
+					}
+					if (side == "WEST") {
+						return indent + d.hwMeta.name;
+					} else if (side == "EAST") {
+						return d.hwMeta.name + indent;
+					} else {
+						throw new Error(side);
+					}
+				} else 
+					return d.hwMeta.name;
+			})
+			.attr("x", function(d) {
+				// var side = d.side;
+				if (d.x < 0) {
+					var side = "WEST"
+				} else {
+					var side = "EAST"
+				}
+				if (side == "WEST") {
+					// Print d.x and d.hwMeta.parent.x to debug
+					console.log(d.x + (this.textContent.length * CHAR_WIDTH) + PORT_WIDTH, d.hwMeta.parent.x);
+					return PORT_WIDTH * 2;
+				} else if (side == "EAST") {
+					// if (typeof this.getBBox == "undefined") {
+						// JSDOM under nodejs
+						return -(this.textContent.length * CHAR_WIDTH) - PORT_WIDTH 
+					// }
+					// return -this.getBBox().width  - PORT_WIDTH;
+				} else if (side == "NORTH") {
+					return 0;
+				} else if (side == "SOUTH") {
+					return 0;
+				} else {
+					throw new Error(side);
+				}
+			})
+			.attr("y", PORT_HEIGHT * 0.75);
 
-        this._applyLayoutLinks();
-    }
-
-    _applyLayoutLinks() {
-        let _this = this;
-        let edges = this._edges;
-
-        let [link, linkWrap, _] = renderLinks(this.root, edges);
-        // build netToLink
-        let netToLink = {};
-        edges.forEach(function (e) {
-            netToLink[getNet(e).id] = {
-                "core": [],
-                "wrap": []
-            };
-        });
-        linkWrap._groups.forEach(function (lg) {
-            lg.forEach(function (l) {
-                let e = d3.select(l).data()[0];
-                netToLink[getNet(e).id]["wrap"].push(l);
-            });
-        });
-        link._groups.forEach(function (lg) {
-            lg.forEach(function (l) {
-                let e = d3.select(l).data()[0];
-                netToLink[getNet(e).id]["core"].push(l);
-            });
-        });
-
-        // set highlingt and tooltip on mouser over over the net
-        linkWrap.on("mouseover", function (ev, d) {
-            let netWrap = netToLink[getNet(d).id]["wrap"];
-            d3.selectAll(netWrap)
-                .classed("link-wrap-activated", true);
-
-            _this.tooltip.show(ev, getNameOfEdge(d));
-        });
-        linkWrap.on("mouseout", function (ev, d) {
-            let netWrap = netToLink[getNet(d).id]["wrap"];
-            d3.selectAll(netWrap)
-                .classed("link-wrap-activated", false);
-
-            _this.tooltip.hide();
-        });
-
-        // set link highlight on net click
-        function onLinkClick(ev, d) {
-            let net = getNet(d);
-            let doSelect = net.selected = !net.selected;
-            // propagate click on all nets with same source
-
-            let netCore = netToLink[net.id]["core"];
-            d3.selectAll(netCore)
-                .classed("link-selected", doSelect);
-            ev.stopPropagation();
-        }
-
-        // Select net on click
-        link.on("click", onLinkClick);
-        linkWrap.on("click", onLinkClick);
-    }
-
-    static fromYosys(yosysJson) {
-        return yosys(yosysJson);
-    }
-
-    terminate() {
-        if (this.layouter) {
-            this.layouter.terminate();
-        }
-    }
-
-    setErrorText(msg) {
-        this.root.selectAll("*").remove();
-        let errText = this.errorText;
-        if (!errText) {
-            errText = this.errorText = this.root.append("text")
-                .attr("x", "50%")
-                .attr("y", "50%")
-                .attr("dominant-baseline", "middle")
-                .attr("text-anchor", "middle")
-                .style("font-size", "34px");
-        }
-        errText.text(msg);
-        let t = d3.zoomTransform(this.root.node());
-        t.k = 1;
-        t.x = 0;
-        t.y = 0;
-        this.root.attr("transform", t);
-
-    }
+		// spot input/output marker
+		portG.append("use")
+			.attr("href", getIOMarker)
+	}
 }
